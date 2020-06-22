57ff5d82f03b51506d3ed8c01c02fa4592004e07
mattostraszewski <mrostraszewski@gmail.com>
2020-06-22 13:52:28 -0400

    created axios call to mount on first render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       pokemon: {},
@@ -10,10 +11,51 @@ class App extends Component {
     }
   }
 
-  render(){
+  componentDidMount = () => {
+    this.getPokemon()
+  }
+
+  getPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2`)
+      .then(res => this.setState({
+        pokemon: res.data
+      }))
+      .catch(err => console.log(err))
+  }
+
+  handleInc = () => {
+    this.setState({
+      pokeId: this.state.pokeId + 1
+    })
+    this.getPokemon()
+  }
+
+  handleDec = () => {
+    this.setState({
+      pokeId: this.state.pokeId - 1
+    })
+    this.getPokemon()
+  }
+
+  render() {
+    const { pokemon, pokeId } = this.state
     return (
       <div className='App'>
-        <img className='pokemon-logo' src='https://1000logos.net/wp-content/uploads/2017/05/Pokemon-Logo.png' alt='Pokemon'/>
+        <img className='pokemon-logo' src='https://1000logos.net/wp-content/uploads/2017/05/Pokemon-Logo.png' alt='Pokemon' />
+
+        {pokemon.name
+          ? (
+            <div>
+              <p>{pokemon.name}</p>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </div>
+          )
+          : null
+        }
+
+        <button onClick={() => this.handleInc} >Next</button>
+        <button onClick={() => this.handleDec}>Prev</button>
       </div>
     )
   }
